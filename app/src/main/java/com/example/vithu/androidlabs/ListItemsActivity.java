2abaeb8cb3cd9af42a4ec00183fe2ef9c50757c4
vithu <srib0001@algonquinlive.com>
2018-11-09 16:12:21 -0500

    done Lab 3

--- app/src/main/java/com/example/vithu/androidlabs/ListItemsActivity.java
+++ app/src/main/java/com/example/vithu/androidlabs/ListItemsActivity.java
@@ -1,13 +1,134 @@
 package com.example.vithu.androidlabs;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.Toast;
+
 
 public class ListItemsActivity extends Activity {
+
+    protected static final String ACTIVITY_NAME = "ListItemsActivity";
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private CharSequence text = "Switch is On";
+    private CharSequence text1 = "Switch is Off";
+    private ImageButton button;
+    private Switch switch1;
+    private CheckBox checkbox;
+    private int requestCode;
+    private int resultCode;
+    private Intent data;
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);
+
+        button = (ImageButton) findViewById(R.id.imagebutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+                }
+            }
+        });
+
+        switch1 = (Switch) findViewById(R.id.switchbox);
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                CharSequence text = "Switch is Off";
+                int duration = Toast.LENGTH_LONG;
+                if (isChecked) {
+                    text = "Switch is On";
+                    duration = Toast.LENGTH_SHORT;
+                    Toast toast = Toast.makeText(ListItemsActivity.this, text, duration);
+                    toast.show();
+
+                } else if (isChecked) {
+                    text = "Switch is Off";
+                    duration = Toast.LENGTH_LONG;
+                    Toast toast = Toast.makeText(ListItemsActivity.this, text, duration);
+                    toast.show();
+                }
+            }
+        });
+        checkbox = (CheckBox) findViewById(R.id.checkbox);
+        checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, final boolean isChecked) {
+                if (isChecked) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(ListItemsActivity.this);
+                    builder.setMessage(R.string.dialog_message);
+                    builder.setTitle(getString(R.string.dialog_text));
+                    builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Intent resultIntent = new Intent();
+                            resultIntent.putExtra("Response", "Here is my response");
+                            setResult(Activity.RESULT_OK, resultIntent);
+                            finish();
+
+                        }
+                    });
+                    builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                        }
+                    });
+                    builder.show();
+                }
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            button.setImageBitmap(imageBitmap);
+        }
+    }
+
+    protected void OnResume(Bundle savedInstance) {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+
+
+    }
+
+    protected void onStart(Bundle savedInstance) {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    protected void onPause(Bundle savedInstance) {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    protected void onStop(Bundle savedInstance) {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    protected void onDestroy(Bundle savedInstance) {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
     }
 }
--- app/src/main/java/com/example/vithu/androidlabs/LoginActivity.java
+++ app/src/main/java/com/example/vithu/androidlabs/LoginActivity.java
@@ -1,13 +1,75 @@
 package com.example.vithu.androidlabs;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
 
+
+    protected static final String ACTIVITY_NAME ="LoginActivity";
+    private Button button;
+    private EditText loginEditText;
+    private SharedPreferences sharedPref;
+    private static final String DEFAULT_EMAIL_KEY = "DEFULT_EMAIL_KEY";
+    private static final String MY_PREF = "MY_PREF";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginEditText = findViewById(R.id.editText);
+        sharedPref = getSharedPreferences(MY_PREF, Context.MODE_PRIVATE);
+        String defaultEmail = sharedPref.getString(DEFAULT_EMAIL_KEY, "email@domain.com");
+
+
+        loginEditText.setText(defaultEmail);
+
+        button = (Button)findViewById(R.id.button_login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = loginEditText.getText().toString();
+
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("DefaultMail", email);
+                editor.commit();
+
+                Intent intent = new Intent(LoginActivity.this, StartActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+    public void callback(){
+
+    }
+    protected void OnResume(Bundle savedInstance){
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+    protected void onStart(Bundle savedInstance) {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    protected void onPause(Bundle savedInstance) {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    protected void onStop(Bundle savedInstance) {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+    protected void onDestroy(Bundle savedInstance) {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
     }
 }
